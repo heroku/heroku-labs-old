3c449233955c01899381e5f451da38289a7ecd49
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-19 22:22:14 -0400

    yeah enable and disable should be puts, we are updating app

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :apps do |app|
-    app.resources :features, :only => :index, :member => { :enable => :post, :disable => :post }
+    app.resources :features, :only => :index, :member => { :enable => :put, :disable => :put }
   end
   map.resources :features, :only => :show
   map.connect ':controller/:action/:id'
