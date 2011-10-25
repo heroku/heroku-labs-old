4a027627492628360018b7bc8b9574dbcd84450c
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-25 12:28:28 -0400

    removed unecessary words from command; leaner and meaner

--- lib/heroku/command/labs.rb
+++ lib/heroku/command/labs.rb
@@ -6,24 +6,9 @@ class Heroku::Command::Labs < Heroku::Command::BaseWithApp
   #
   def index
     features = heroku.list_features(app)
-    if features.empty?
-      display " You are currently not enrolled in our labs program."
-      display " If you would like to participate, and are willing"
-      display " to commit to providing helpful feedback and testing"
-      display " new features as they are released, send an email to"
-      display " labs@heroku.com. Include an explanation of why you"
-      display " would make a good tester, and what features you are"
-      display " interested in."
-    else
-      lines = features.map do |feature|
-        row = [feature['enabled'] ? '*' : ' ', feature['name']]
-        row.join(' ')
-      end
-      display lines.join("\n")
-      display
-      display " 'heroku labs:info feature' to see the details of a feature"
-      display " 'heroku labs:enable feature --app myapp' to enable a feature"
-      display " 'heroku labs:disable feature --app myapp' to disable a features"
+    lines = features.map do |feature|
+      row = [feature['enabled'] ? '*' : ' ', feature['name']]
+      row.join(' ')
     end
   end
 
