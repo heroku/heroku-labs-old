362625f95fddea42811809c6e55fafa61cd688d0
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-25 12:27:33 -0400

    added all the trimmings to the model (callbacks, validation)

--- core/app/models/feature.rb
+++ core/app/models/feature.rb
@@ -1,8 +1,17 @@
 class Feature < ActiveRecord::Base
+  validates_presence_of :name, :details, :documentation, :support, :flag
+  before_save :ensure_case
   
   named_scope :by_name, lambda { |name| { :conditions => [ "lower(name) = ?", name.downcase ] } }
   
   def to_param
     name.parameterize
+  end
+  
+  private
+  
+  def ensure_case
+    self.name.downcase!
+    self.flag.downcase!
   end
 end
